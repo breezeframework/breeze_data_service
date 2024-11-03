57a2f14a2a1c824b60003512cdf37efe2866aead
Kosha <igornem1@gmail.com>
2024-11-03 17:15:11 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module breeze_data_service
+
+go 1.23.2
+
+require github.com/breezeframework/breeze_data v0.0.0-20241103133956-d1403c42f4ff
+
+require (
+	github.com/Masterminds/squirrel v1.5.4 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
---
+++ service/CrudService.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+	"github.com/breezeframework/breeze_data/repository"
+)
+
+type CrudService[T any] interface {
+	CreateEntity(ctx context.Context, entity T) (int64, error)
+	GetById(ctx context.Context, id int64) (T, error)
+	GetAll(ctx context.Context) (*[]T, error)
+	Update(ctx context.Context, id int64, entity T) error
+	Delete(ctx context.Context, id int64) error
+}
+
+type GenericService[T any] struct {
+	repo repository.CrudRepository[T]
+}
+
+func NewGenericService[T any](repo repository.CrudRepository[T]) *GenericService[T] {
+	return &GenericService[T]{repo: repo}
+}
+
+func (s *GenericService[T]) Create(ctx context.Context, entity T) (int64, error) {
+	return s.repo.Create(ctx, entity)
+}
+
+func (s *GenericService[T]) GetById(ctx context.Context, id int64) (*T, error) {
+	return s.repo.GetById(ctx, id)
+}
+
+func (s *GenericService[T]) GetAll(ctx context.Context) (*[]T, error) {
+	return s.repo.GetAll(ctx)
+}
+
+func (s *GenericService[T]) Update(ctx context.Context, id int64, entity T) error {
+	return s.repo.Update(ctx, id, entity)
+}
+
+func (s *GenericService[T]) Delete(ctx context.Context, id int64) error {
+	return s.repo.Delete(ctx, id)
+}
