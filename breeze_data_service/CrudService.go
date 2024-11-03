fb60a8a64a0ee8050341f5a80e2f2e54fba78992
Kosha <igornem1@gmail.com>
2024-11-04 01:15:00 +0200

    initial commit

--- breeze_data_service/CrudService.go
+++ breeze_data_service/CrudService.go
@@ -2,13 +2,15 @@ package breeze_data_service
 
 import (
 	"context"
+	"github.com/Masterminds/squirrel"
 	"github.com/breezeframework/breeze_data/breeze_data"
 )
 
 type CrudService[T any] interface {
-	GetById(ctx context.Context, id int64) (T, error)
+	GetById(ctx context.Context, id int64) (*T, error)
+	GetBy(ctx context.Context, where squirrel.Eq) (*[]T, error)
 	GetAll(ctx context.Context) (*[]T, error)
-	CreateEntity(ctx context.Context, entity T) (int64, error)
+	Create(ctx context.Context, entity T) (int64, error)
 	Update(ctx context.Context, id int64, entity T) error
 	Delete(ctx context.Context, id int64) error
 }
@@ -17,7 +19,7 @@ type GenericService[T any] struct {
 	Repo breeze_data.CrudRepository[T]
 }
 
-func NewGenericService[T any](repo breeze_data.CrudRepository[T]) *GenericService[T] {
+func NewGenericService[T any](repo breeze_data.CrudRepository[T]) CrudService[T] {
 	return &GenericService[T]{Repo: repo}
 }
 
@@ -30,6 +32,9 @@ func (s *GenericService[T]) GetById(ctx context.Context, id int64) (*T, error) {
 }
 
 func (s *GenericService[T]) GetAll(ctx context.Context) (*[]T, error) {
+	return s.Repo.GetAll(ctx)
+}
+func (s *GenericService[T]) GetBy(ctx context.Context, where squirrel.Eq) (*[]T, error) {
 	return s.Repo.GetAll(ctx)
 }
 
