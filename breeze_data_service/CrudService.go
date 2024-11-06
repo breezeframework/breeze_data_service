a3e3fccea66a4879e69e421ce8977f0f07f807cb
Kosha <igornem1@gmail.com>
2024-11-06 16:23:39 +0200

    next commit

--- breeze_data_service/CrudService.go
+++ breeze_data_service/CrudService.go
@@ -16,32 +16,9 @@ type CrudService[T any] interface {
 }
 
 type CrudServiceImpl[T any] struct {
-	repo breeze_data.CrudRepository[T]
+	breeze_data.CrudRepository[T]
 }
 
 func NewCrudService[T any](repo breeze_data.CrudRepository[T]) CrudServiceImpl[T] {
-	return CrudServiceImpl[T]{repo: repo}
-}
-
-func (s *CrudServiceImpl[T]) Create(ctx context.Context, entity T) (int64, error) {
-	return s.repo.Create(ctx, entity)
-}
-
-func (s *CrudServiceImpl[T]) GetById(ctx context.Context, id int64) (*T, error) {
-	return s.repo.GetById(ctx, id)
-}
-
-func (s *CrudServiceImpl[T]) GetAll(ctx context.Context) (*[]T, error) {
-	return s.repo.GetAll(ctx)
-}
-func (s *CrudServiceImpl[T]) GetBy(ctx context.Context, where squirrel.Eq) (*[]T, error) {
-	return s.repo.GetBy(ctx, where)
-}
-
-func (s *CrudServiceImpl[T]) Update(ctx context.Context, id int64, entity T) error {
-	return s.repo.Update(ctx, id, entity)
-}
-
-func (s *CrudServiceImpl[T]) Delete(ctx context.Context, id int64) error {
-	return s.repo.Delete(ctx, id)
+	return CrudServiceImpl[T]{repo}
 }
